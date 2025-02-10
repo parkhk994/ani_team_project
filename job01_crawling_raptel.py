0bc7c245a70e2e82082f014f6b1112189dd7d2f8
parkhk994 <parkhk994@gmail.com>
2025-02-10 17:09:08 +0900

    라프텔 v1

--- job01_crawling_raptel.py
+++ job01_crawling_raptel.py
@@ -35,7 +35,8 @@ def open_in_new_tab(driver, element):
     driver.switch_to.window(driver.window_handles[-1])
 
 def srolling():
-    for _ in range(10):  # 50번 페이지 다운 시도
+    # for _ in range(10):  # 50번 페이지 다운 시도
+    for _ in range(5):  # 50번 페이지 다운 시도
         # 페이지의 body 요소를 찾아서 end 키를 보냄
         driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.END)
         time.sleep(1)
@@ -50,7 +51,7 @@ driver.get("https://laftel.net/finder")
 body = driver.find_element(By.TAG_NAME, "body")
 time.sleep(1)
 
-sleep_sec = 3
+sleep_sec = 2
 
 # 인기 순 버튼
 button_CSS_pop = '#root > div.sc-f0aad20d-0.cJKdpk > div.sc-314e188f-0.hqsXVO > div.sc-314e188f-2.hVHfbj > div > div > div > div.simplebar-wrapper > div.simplebar-mask > div > div > div > div.sc-b650993-1.dPuSyZ > div.sc-42a3c8f1-0.eiatWD > div > div > div.sc-29d1736d-0.fkHAgS > div > div'
@@ -80,14 +81,20 @@ time.sleep(sleep_sec)
 srolling()
 # 홈키 클릭
 driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.HOME)
-time.sleep(1)
+time.sleep(sleep_sec)
 
-# for i in range(1, 200):
-for i in range(1, 3):
+for i in range(1, 200):
+# for i in range(1, 3):
     try:
         TitleTap_Xpath = f'//*[@id="root"]/div[2]/div[2]/div[2]/div/div/div/div[1]/div[2]/div/div/div/div[2]/div/a[{i}]/div'
-        driver.find_element(By.XPATH, TitleTap_Xpath).click()
-        time.sleep(1)
+        # driver.find_element(By.XPATH, TitleTap_Xpath).click()
+        # time.sleep(sleep_sec)
+
+        # 새 탭에서 책 열기
+        element = driver.find_element(By.XPATH, TitleTap_Xpath)
+        open_in_new_tab(driver, element)
+        print(i)
+        time.sleep(sleep_sec)
 
         # 타이틀 추출
         title_xpath = '//*[@id="item-modal"]/div[1]/div[2]/div/header/h1'
@@ -105,10 +112,11 @@ for i in range(1, 3):
 
         ReviewsTap_Xpath = '//*[@id="item-tab-view"]/div[1]/div/div/div[1]/a[2]'
         driver.find_element(By.XPATH, ReviewsTap_Xpath).click()
-        time.sleep(1)
+        time.sleep(sleep_sec)
 
-        [(driver.find_element(By.XPATH, '//*[@id="item-tab-view"]').click(),
-          time.sleep(0.5), body.send_keys(Keys.END), time.sleep(0.5)) for _ in range(4)] # 리뷰창 스크롤 4번
+        # [(driver.find_element(By.XPATH, '//*[@id="item-tab-view"]').click(),
+        #   time.sleep(0.5), body.send_keys(Keys.END), time.sleep(0.5)) for _ in range(4)] # 리뷰창 스크롤 4번
+        srolling()
 
         #  for j 루프 들여쓰기 수정
         for j in range(1, 50):
@@ -118,14 +126,20 @@ for i in range(1, 3):
                 Review = re.compile('[^가-힣 ]').sub('', Review)  # 한글만 남김
                 Reviews.append(Review)
                 print(Review)
-                time.sleep(0.5)
+                # time.sleep(0.5)
 
             except NoSuchElementException:
                 print(f"[{j}] 번째 없음 건너뜀")
                 continue  # 다음 j 값으로 넘어감
+        # input("멈춤")
+        # body.send_keys(Keys.ESCAPE)  # ESC 키 입력 (닫기)
+        # time.sleep(sleep_sec)
+        # 현재 탭(새로 열린 탭) 닫기
+        driver.close()
 
-        body.send_keys(Keys.ESCAPE)  # ESC 키 입력 (닫기)
-        time.sleep(1)
+        # 원래 탭으로 돌아가기
+        driver.switch_to.window(driver.window_handles[0])
+        time.sleep(sleep_sec)
 
     except NoSuchElementException:
         print(f"[{i}] 번째 없음 건너뜀")
